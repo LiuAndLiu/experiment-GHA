b76dc2b7475f836829321cc4cabee2421eeedf5c
Justin <jerwei.yeoh@exwzd.com>
2021-08-20 09:18:36 +0900

    Test secret

---
+++ main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    github = {
+      source = "integrations/github"
+      version = "4.13.0"
+    }
+  }
+}
+
+variable liuandliu_token {
+  type        = string
+  description = "description"
+}
+
+
+provider "github" {
+    owner = "liuandliu"
+    token = var.liuandliu_token
+}
+
+resource "github_repository" "test" {
+    name = "experiment-GHA"
+
+    lifecycle {
+        ignore_changes = [
+            allow_merge_commit,
+            allow_rebase_merge,
+            allow_squash_merge,
+            has_downloads,
+            has_issues,
+            has_projects,
+            has_wiki,
+        ]
+    }
+}
+
+data "github_actions_public_key" "example_public_key" {
+  repository = github_repository.test.name
+}
+
+output "public_key" {
+    value = data.github_actions_public_key.example_public_key
+}
+
+resource "github_actions_secret" "secret_test" {
+    repository = github_repository.test.name
+    secret_name = "test"
+    encrypted_value = "ykaUjBBif/vnCXjxD0D2fkYStjDEbduOCTGNdziVzSvMByjzuIw3unL1QLVZeOODQ8wQQA=="
+}
+
+resource "github_actions_secret" "secret_test_2" {
+    repository = github_repository.test.name
+    secret_name = "test2"
+    plaintext_value = "dGVzdDIK"
+}
